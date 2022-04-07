89bd0f50a17c74f6896d890810d36c03df11ac45
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2022-04-07 10:29:36 +0100

    demo video added

--- WeatherApp/WeatherApp/ViewModel/WeatherPageViewModel.swift
+++ WeatherApp/WeatherApp/ViewModel/WeatherPageViewModel.swift
@@ -7,6 +7,42 @@
 
 import Foundation
 
+protocol DataDelegate {
+    func fetchCurrentDayData(_ data: CurrentDayWeatherData)
+    func fetchFiveDayData(_ data: FiveDaysWeatherData)
+}
+
+
 class WeatherPageViewModel {
     
+    var delegate: DataDelegate?
+    var currentDay: CurrentDayWeatherData?
+    let apiOne : WeatherApiOne = WeatherApiOne.sharedInstance
+    let apiTwo : WeatherApiTwo = WeatherApiTwo.sharedInstance
+    var fiveDaysData: FiveDaysWeatherData?
+    
+    func fetchCurrentDayData() {
+        apiOne.dataParser{ [weak self] result in
+            switch result{
+            case .success(let results):
+                DispatchQueue.main.async {
+                    self?.delegate?.fetchCurrentDayData(results)
+                }
+            case .failure(let errors):
+                print(errors)
+            }
+        }
+    }
+    func fetchFiveDayData() {
+        apiTwo.dataParser{ [weak self] result in
+            switch result {
+            case .success(let fiveDaysWeather):
+                DispatchQueue.main.async {
+                    self?.delegate?.fetchFiveDayData(fiveDaysWeather)
+                }
+            case .failure(let errors):
+                print(errors)
+            }
+        }
+    }
 }
